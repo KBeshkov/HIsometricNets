ecfc79abeb5a0523804bc69f51716bb7a038e803
Kosio Beshkov <kosio@eduroam-193-157-168-230.wlan.uio.no>
2024-01-17 14:01:09 +0100

    update

--- Results/HRIM_CIFAR10.py
+++ Results/HRIM_CIFAR10.py
@@ -17,25 +17,25 @@ sys.path.append(os.getcwd()[:-7]+'Analysis')
 from Algorithms import *
 from metrics import *
 from ML_models import *
-
+from torchviz import make_dot
 
 #%%
-sys.path.append("C:\Repos\PyTorch_CIFAR10\\")
+sys.path.append("/Users/kosio/Repos/PyTorch_CIFAR10/")
 from cifar10_models.vgg import vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
 
 # Pretrained model
-feature_model = vgg13_bn(pretrained=True)
-feature_model.eval() # for evaluation
+# feature_model = vgg13_bn(pretrained=True)
+# feature_model.eval() # for evaluation
 
-feature_model = CNN_Features(first_layer_output=32)
+feature_model = CNN_Features(first_layer_output=128).double()
 #%%
 Iso_coef = 0.01
 
 transform=transforms.Compose([transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
 
-data_train = datasets.CIFAR10('E:\Data\HIsom_Data', train=True, transform=transform, download=True)
-data_test = datasets.CIFAR10('E:\Data\HIsom_Data', train=False, transform=transform, download=True)
+data_train = datasets.CIFAR10('/Users/kosio/Repos/HIsometricNets/Data/', train=True, transform=transform, download=True)
+data_test = datasets.CIFAR10('/Users/kosio/Repos/HIsometricNets/Data/', train=False, transform=transform, download=True)
 
 
 data_train_hierarchy = HierarchicalDataset(data_train,conv=True)
@@ -49,7 +49,7 @@ data_part_test = data_test_hierarchy.__hierarchy__(partition)
 hierarchical_trainloader = torch.utils.data.DataLoader(data_train_hierarchy, batch_size=100, shuffle=True)
 hierarchical_testloader = torch.utils.data.DataLoader(data_test_hierarchy, batch_size=100, shuffle=True)
   
-precomputed_dmat = 'csv'
+precomputed_dmat = 'standard'
 if precomputed_dmat == True:
     CIFAR_dmat = torch.load('E:\Data\HIsom_Data\CIFAR10_dmat_features.pt').double() #Precomputed distance matrix between MNIST images
 elif precomputed_dmat=='standard':
@@ -58,21 +58,21 @@ elif precomputed_dmat=='csv':
     CIFAR_dmat = 'csv:E:/Data/HIsom_Data/CIFAR10_dmat.csv'
 else:
     CIFAR_dmat='iterate'
-res = int(2*(data_train[0][0].size()[-1]/2)**2)
+res = int(2*(data_train[0][0].size()[-1]/2)**2)*32
 
 #%% Train Model
 nlayers = 3
 nclass = len(partition)
-n_nrns = 100
+n_nrns = 1000
 network_params = {'n_neurons': n_nrns, 'n_inputs': res,'n_classes': 10,
                   'projection': [],'weights': [],'n_layers': nclass*nlayers,'feature_model':feature_model}
 HRIM = HierarchicalRepresentationNetwork(NN_class(**network_params).double().to('cpu'),savepoints=25,device='cpu')
 
 #%%
-K = 0.01
+K = 1
 costs = [[nn.CrossEntropyLoss(),MetricPreservationLoss(Iso_coef, K).Loss]]
 
-HRIM_data = HRIM.train(hierarchical_trainloader, costs, dmat=CIFAR_dmat,epochs=50)
+HRIM_data = HRIM.train(hierarchical_trainloader, costs, dmat=CIFAR_dmat,epochs=10)
 
 #%% Test model
 HRIM_test = HRIM.test(hierarchical_testloader)
@@ -93,11 +93,34 @@ for e in epsilons:
                                                      epsilon=e, attack_method=attack_type,featurize=True))
 
 class_performance = np.hstack([adv_attacks[i][0] for i in range(len(epsilons))]).T
-orig_images1 = [np.reshape(adv_attacks[i][1][-1][2][5],[28,28]) for i in range(len(epsilons))]
+orig_images1 = [adv_attacks[i][1][-1][2][5].T for i in range(len(epsilons))]
 orig_labels1 = [adv_attacks[i][1][-1][0][5] for i in range(len(epsilons))]
-orig_images2 = [np.reshape(adv_attacks[i][1][-1][2][3],[28,28]) for i in range(len(epsilons))]
+orig_images2 = [adv_attacks[i][1][-1][2][3].T for i in range(len(epsilons))]
 orig_labels2 = [adv_attacks[i][1][-1][0][3] for i in range(len(epsilons))]
-pert_images1 = [np.reshape(adv_attacks[i][1][-1][3][5],[28,28]) for i in range(len(epsilons))]
+pert_images1 = [adv_attacks[i][1][-1][3][5].T for i in range(len(epsilons))]
 pert_labels1 = [adv_attacks[i][1][-1][1][5] for i in range(len(epsilons))]
-pert_images2 = [np.reshape(adv_attacks[i][1][-1][3][3],[28,28]) for i in range(len(epsilons))]
+pert_images2 = [adv_attacks[i][1][-1][3][3].T for i in range(len(epsilons))]
 pert_labels2 = [adv_attacks[i][1][-1][1][3] for i in range(len(epsilons))]
+saliency_maps1 = [saliency_map(torch.Tensor(orig_images1[i]).T[None], HRIM.model, orig_labels1[i],threshold=0.3) for i in range(len(epsilons))]
+saliency_maps2 = [saliency_map(torch.Tensor(orig_images2[i]).T[None], HRIM.model, orig_labels2[i],threshold=0.3) for i in range(len(epsilons))]
+
+#%%
+fig, ax = plt.subplots(6,len(epsilons))
+
+
+for j in range(len(epsilons)):
+    ax[0,j].imshow(orig_images1[j])
+    ax[1,j].imshow(orig_images1[j])
+    ax[1,j].pcolormesh(saliency_maps1[j][0].T,alpha=1,cmap='coolwarm',shading='auto')
+    ax[2,j].imshow(pert_images1[j])
+    ax[3,j].imshow(orig_images2[j])
+    ax[4,j].imshow(orig_images2[j])
+    ax[4,j].pcolormesh(saliency_maps2[j][0].T,alpha=1,cmap='coolwarm',shading='auto')
+    ax[5,j].imshow(pert_images2[j])
+for cur_ax in ax:
+    for cur_ax in cur_ax:
+        cur_ax.axis('off')
+
+fig.tight_layout()
+
+fig.savefig('../Figures/images_'+str(Iso_coef)+'_CIFAR.png',dpi=500)
