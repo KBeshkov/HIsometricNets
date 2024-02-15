Fixed restricted backprop through convolutional layers

Analysis/ML_models.py
@@ -48,7 +48,8 @@ class NN_class(nn.Module):
             #     param.requires_grad = False
 
         self.layers = nn.ModuleList()  # []
-        self.inp_layer = nn.Linear(n_inputs, n_neurons, bias=True)
+        inp_layer = nn.Linear(n_inputs, n_neurons, bias=True)
+        self.layers.append(inp_layer.double())
         if len(self.projection) > 0:
             self.inp_layer.weight.data = projection
         self.act_fun = nn.Tanh()  #nn.ReLU()# torch.sin##nn.Hardtanh()#
@@ -64,18 +65,18 @@ class NN_class(nn.Module):
     def forward(self, x):
         if self.feature_model != None:
             x = self.feature_model(x.double())
-            x = x.reshape(len(x),-1)
-        out1 = self.act_fun(self.inp_layer(torch.squeeze(x.double())))
+            x = torch.squeeze(x.reshape(len(x),-1))
+        else:
+            x = torch.squeeze(x.double())
         for layer in self.layers:
-            out1 = self.act_fun(layer(out1))
-        out2 = self.class_layer(out1)
-        return out1, out2
+            x = self.act_fun(layer(x))
+        out = self.class_layer(x)
+        return x, out
 
     def lim_forward(self, x, layer_stop):
-        out1 = self.act_fun(self.inp_layer(x.double()))
         for layer in self.layers[:-layer_stop]:
-            out1 = self.act_fun(layer(out1))
-        return out1
+            x = self.act_fun(layer(x.double()))
+        return x
 
 class CNN_Features(nn.Module):
     def __init__(self, num_conv_layers=2, in_channels=3, first_layer_output=16, num_classes=10):
@@ -173,7 +174,7 @@ class MetricPreservationLoss(nn.Module):
         crit1 = nn.MSELoss()
         # loss = self.lambda1*crit1(Y*est_dmats/(self.lambda2*torch.max(Y*est_dmats)),Y*y1_dmats/torch.max(Y*y1_dmats))**2
         loss = self.lambda1 * crit1((self.lambda2 * Y * est_dmats).float(), (Y * y_dmats).float())
-        return loss, (torch.max(Y * y_dmats) / torch.max(Y * est_dmats)).item()
+        return loss
 
 
 def pair_labels(labels):
@@ -195,7 +196,7 @@ def train(net, train_dat, optimizer, criterion_weights, dmat, epochs, batch_sz=2
     labels = torch.tensor(train_dat[1][batch_ind].type(torch.LongTensor))
     labels_d = pair_labels(labels)
     save_outputs = []
-    Lipschitz_constants = []
+    
     criterion1 = nn.CrossEntropyLoss()
     criterion2 = MetricPreservationLoss(lambda1=criterion_weights[1], lambda2=1).Loss    
     pbar = tqdm(range(epochs),ncols=150)
@@ -209,8 +210,7 @@ def train(net, train_dat, optimizer, criterion_weights, dmat, epochs, batch_sz=2
             # forward + backward + optimize
             outputs1, outputs2 = net(inputs)
             loss1 = criterion_weights[0] * criterion1(outputs2, labels)
-            loss2, c = criterion2(outputs1, dmat, labels_d)
-            Lipschitz_constants.append(c)
+            loss2 = criterion2(outputs1, dmat, labels_d)
             loss = loss1 + loss2
             loss.backward()
             optimizer.step()
@@ -225,8 +225,7 @@ def train(net, train_dat, optimizer, criterion_weights, dmat, epochs, batch_sz=2
         if epoch % 50 == 0 or epoch == 0:
             save_outputs.append(outputs1)
 
-    return loss_curve, loss1_curve, loss2_curve, save_outputs, Lipschitz_constants
-
+    return loss_curve, loss1_curve, loss2_curve, save_outputs
 
 def predict(net, test_dat):
     n_classes = net.n_classes
@@ -311,20 +310,20 @@ class HierarchicalRepresentationNetwork:
         if self.model.feature_model!=None:
             x = self.model.feature_model(x.double())
             x = x.reshape(len(x),-1)
-        out = self.model.act_fun(self.model.inp_layer(x.double()))
         for layer in self.model.layers[:stop_layer]:
-            out = self.model.act_fun(layer(out))
-        class_out = self.model.class_layer(out)
-        return out, class_out
+            x = self.model.act_fun(layer(x))
+        class_out = self.model.class_layer(x)
+        return x, class_out
 
     def restricted_backward_pass(self, loss, stop_layer):
         count_layer = 0
-        for params in self.model.parameters():
-            params.requires_grad = False
-            if count_layer == stop_layer:
-                break
-            if len(params.size()) == 2:
-                count_layer += 1
+        if stop_layer!=0:
+            for params in self.model.parameters():
+                params.requires_grad = False
+                if count_layer == stop_layer:
+                    break
+                if len(params.size()) == 2: #takes care of removing convolutional feature layers
+                    count_layer += 1
         loss.backward()
         return
 
@@ -343,7 +342,7 @@ class HierarchicalRepresentationNetwork:
         running_loss1 = [[] for l in range(len(labels))]
         running_loss2 = [[] for l in range(len(labels))]
         mflds_train = []
-        Lipschitz_constants = []
+        
         optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
         if dmat == "standard":
             dmat = torch.cdist(train_data, train_data).to(self.device)
@@ -371,14 +370,13 @@ class HierarchicalRepresentationNetwork:
                     loss1 = costs[branch][0](outs[1], targets[branch].long())
                     if costs[branch][1] != 0:
                         if dmat == "iterate":
-                            loss2, c = costs[branch][1](outs[0], dmat_temp, labels_d)
+                            loss2 = costs[branch][1](outs[0], dmat_temp, labels_d)
                         elif dmat[:3] == "csv":
-                            loss2, c = costs[branch][1](outs[0], dmat_temp, labels_d)
+                            loss2 = costs[branch][1](outs[0], dmat_temp, labels_d)
                         else:
-                            loss2, c = costs[branch][1](
+                            loss2 = costs[branch][1](
                                 outs[0], dmat[indices, :][:, indices], labels_d
                             )
-                        Lipschitz_constants.append(c)
                     else:
                         loss2 = 0
                     loss = loss1 + loss2
@@ -398,7 +396,7 @@ class HierarchicalRepresentationNetwork:
                         ].append(round(loss2.item(),2))
                         
 
-                    pbar.set_description(f"Epoch {ep}, batch {batch_n}. Loss = {tot_loss}; {first_loss} CSE; {second_loss} Isometric. Classification {performance}")
+                    pbar.set_description(f"Ep {ep}, btch {batch_n}. Loss={tot_loss}; {first_loss} CSE; {second_loss} Iso. Class {performance}")
 
 
             tot_loss = [branch_loss[-1] for branch_loss in running_loss]
@@ -428,7 +426,7 @@ class HierarchicalRepresentationNetwork:
                 plt.xscale("log"), plt.yscale("log")
             plt.tight_layout()
         mflds_train.append(forward_runs)
-        return mflds_train, Lipschitz_constants
+        return mflds_train
 
     def test(self, data_loader):
         labels = data_loader.dataset.labels
@@ -441,7 +439,7 @@ class HierarchicalRepresentationNetwork:
                     torch.sum(torch.argmax(out, 1) == targets[branch])
                 )
         performance = [
-            sum(performance[i]) / data_loader.dataset.__len__()
+            (sum(performance[i]) / data_loader.dataset.__len__()).item()
             for i in range(len(labels))
         ]
         return performance
@@ -473,7 +471,7 @@ class HierarchicalRepresentationNetwork:
         # Calculate the loss
         loss1 = costs[0](out[1], targets)
         if costs[1] != 0:
-            loss2, _ = costs[1](out[0], dmat, labels_d)
+            loss2 = costs[1](out[0], dmat, labels_d)
         else:
             loss2 = 0
         loss = loss1 + loss2
@@ -504,7 +502,7 @@ class HierarchicalRepresentationNetwork:
             # Calculate the loss
             loss1 = costs[0](prediction[1], targets)
             if costs[1] != 0:
-                loss2, _ = costs[1](prediction[0], dmat, labels_d)
+                loss2 = costs[1](prediction[0], dmat, labels_d)
             else:
                 loss2 = 0
             loss = loss1 + loss2

Results/HRIM_CIFAR10.py
@@ -29,13 +29,13 @@ from cifar10_models.vgg import vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
 
 feature_model = CNN_Features(first_layer_output=128).double()
 #%%
-Iso_coef = 2
+Iso_coef = 0.1
 
 transform=transforms.Compose([transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616))])
 
 data_train = datasets.CIFAR10('/Users/kosio/Repos/HIsometricNets/Data/', train=True, transform=transform, download=True)
-data_test = datasets.CIFAR10('/Users/kosio/Repos/HIsometricNets/Data/', train=False, transform=transform, shuffle=False, download=True)
+data_test = datasets.CIFAR10('/Users/kosio/Repos/HIsometricNets/Data/', train=False, transform=transform, download=True)
 
 
 data_train_hierarchy = HierarchicalDataset(data_train,conv=True)
@@ -47,7 +47,7 @@ data_part_train = data_train_hierarchy.__hierarchy__(partition)
 data_part_test = data_test_hierarchy.__hierarchy__(partition)
 
 hierarchical_trainloader = torch.utils.data.DataLoader(data_train_hierarchy, batch_size=100, shuffle=True)
-hierarchical_testloader = torch.utils.data.DataLoader(data_test_hierarchy, batch_size=100, shuffle=True)
+hierarchical_testloader = torch.utils.data.DataLoader(data_test_hierarchy, batch_size=100, shuffle=False)
   
 precomputed_dmat = 'standard'
 if precomputed_dmat == True:
@@ -103,6 +103,16 @@ pert_images2 = [adv_attacks[i][1][-1][3][3].T for i in range(len(epsilons))]
 pert_labels2 = [adv_attacks[i][1][-1][1][3] for i in range(len(epsilons))]
 saliency_maps1 = [saliency_map(torch.Tensor(orig_images1[i]).T[None], HRIM.model, orig_labels1[i],threshold=0.3) for i in range(len(epsilons))]
 saliency_maps2 = [saliency_map(torch.Tensor(orig_images2[i]).T[None], HRIM.model, orig_labels2[i],threshold=0.3) for i in range(len(epsilons))]
+
+#%%
+saliency_maps = [saliency_map(torch.Tensor(adv_attacks[0][1][-1][2][i].T).T[None], 
+                              HRIM.model, hierarchical_testloader.dataset.labels[0][i],
+                              threshold=0) for i in range(len(adv_attacks[0][1][-1][2]))]
+
+laplacian_distributions = [laplacian_filter(saliency_maps[i].T).flatten() for i in range(len(saliency_maps))]
+total_distrib = np.vstack(laplacian_distributions).flatten()
+
+np.save('../Data/laplace_smooth'+str(Iso_coef)+'CIFAR10.npy',total_distrib)
 
 #%%
 fig, ax = plt.subplots(6,len(epsilons))

Results/HRIM_ICAN21.py
@@ -30,12 +30,12 @@ from cifar10_models.vgg import vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
 
 feature_model = CNN_Features(first_layer_output=128).double()
 #%%
-Iso_coef = 1
+Iso_coef = 0.0001
 
 transform=transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
 
-data_train = InatCustom('/Users/kosio/Repos/HIsometricNets/Data/inat-2021/semi-inat-2021-mini/train/', transform=transform)
-data_test = InatCustom('/Users/kosio/Repos/HIsometricNets/Data/inat-2021/semi-inat-2021-mini/test/', transform=transform)
+data_train = InatCustom('/Users/kosio/Repos/HIsometricNets/Data/inat-2021/semi-inat-2021/train/train/', transform=transform)
+data_test = InatCustom('/Users/kosio/Repos/HIsometricNets/Data/inat-2021/semi-inat-2021/test/test/', transform=transform)
 
 
 data_train_hierarchy = HierarchicalDataset(data_train,conv=True,scale_img=False)
@@ -65,33 +65,41 @@ res = int(2*(data_train[0][0].size()[-1]/2)**2)*2048*8
 #%%
 nlayers = 3
 nclass = len(partition)
-n_nrns = 100
-network_params = {'n_neurons': n_nrns, 'n_inputs': res,'n_classes': 31,
+n_nrns = 200
+network_params = {'n_neurons': n_nrns, 'n_inputs': res,'n_classes': len(set(data_train.targets)),
                   'projection': [],'weights': [],'n_layers': nclass*nlayers,'feature_model':feature_model}
 HRIM = HierarchicalRepresentationNetwork(NN_class(**network_params).double().to('cpu'),savepoints=25,device='cpu')
-#%%
+
+#%%Train or load pretrained model
 K = 1
 costs = [[nn.CrossEntropyLoss(),MetricPreservationLoss(Iso_coef, K).Loss] for i in range(len(partition))]
 
-HRIM_data = HRIM.train(hierarchical_trainloader, costs, dmat=Ican_dmat,epochs=200)
+# HRIM.model.load_state_dict(torch.load("/Users/kosio/Data/HRIM_ICAN.pt",map_location = torch.device('cpu')))
+HRIM_data = HRIM.train(hierarchical_trainloader, costs, dmat=Ican_dmat,epochs=20)
 
 #%% Test model
 HRIM_test = HRIM.test(hierarchical_testloader)
 print(HRIM_test)
 
 #%% Testing adverserial attacks
-orig_images =hierarchical_testloader.dataset.x
-saliency_maps = HRIM.saliency_map(orig_images, hierarchical_testloader.dataset.labels, threshold=0.001)
+first_n = 100
+orig_images = hierarchical_testloader.dataset.x[:first_n]
+labels_images = [hierarchical_testloader.dataset.labels[i][:first_n] for i in range(len(hierarchical_testloader.dataset.labels))]
+saliency_maps = HRIM.saliency_map(orig_images, labels_images, threshold=0.01)
 
 
 #%%
-n_images = 8
+n_images = 3
+rand_images = np.random.choice(np.arange(0,len(orig_images)),replace=False,size=n_images)
+hierarchies = ["kingdom","phylum","class","order","family","genus","species"]
 fig, ax = plt.subplots(n_images,len(partition))
 
-for i in range(n_images):
+for i,img_id in enumerate(rand_images):
     for j in range(len(partition)):
-        ax[i,j].imshow(orig_images[i].detach().numpy().T)
-        ax[i,j].pcolormesh(saliency_maps[j][i].T,alpha=0.5,cmap='coolwarm',shading='auto')
+        # ax[i,j].imshow(orig_images[img_id].detach().numpy().T)
+        ax[i,j].pcolormesh(saliency_maps[j][img_id].T,alpha=0.4,cmap='coolwarm',shading='auto')
+        if i==0:
+            ax[i,j].set_title(hierarchies[j])
     for cur_ax in ax:
         for cur_ax in cur_ax:
             cur_ax.axis('off')